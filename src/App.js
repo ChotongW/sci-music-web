eb3121c85e2b2fec4a73c8ed231a7ade9fdcf0d5
Wasin Wasantivong <chotongza@gmail.com>
2022-11-07 23:28:37 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import Homepage from "./components/Homepage";
+function MyButton() {
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+  }
+  return <button onClick={handleClick}>{count} timesLet's start</button>;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Homepage />
+      <MyButton />
     </div>
   );
 }
---
+++ src/components/Homepage.js
@@ -0,0 +1,10 @@
+function Homepage() {
+  const design = { color: "blue", textAlign: "center" };
+  return (
+    <>
+      <h1 style={design}>Welcome to Web Sci Music Homepage</h1>
+    </>
+  );
+}
+
+export default Homepage;
